18b84c2919d20bc43abbab096459dcfa4715ebdc
Mikhail <mihkailmatveichuk@gmail.com>
2022-12-04 19:55:45 +0300

    feat: change style.css file

--- src/components/app/app.ts
+++ src/components/app/app.ts
@@ -13,7 +13,7 @@ class App {
     start() {
         document
             .querySelector('.sources')
-            ?.addEventListener('click', (e) => this.controller.getNews(e, (data: any) => this.view.drawNews(data)));
+            ?.addEventListener('click', (e) => this.controller.getNews(e, (data) => this.view.drawNews(data)));
         this.controller.getSources((data) => this.view.drawSources(data));
     }
 }
