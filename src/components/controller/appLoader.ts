a1251a794c2054c200a3c6e96482af1d2f04cb0a
Mikhail <mihkailmatveichuk@gmail.com>
2022-12-10 19:52:35 +0300

    refactor: change url

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -3,7 +3,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.up.railway.app/', {
             apiKey: '9e58e025500941d3ab5fdca9b0e3b59c', // получите свой ключ https://newsapi.org/
         });
     }
