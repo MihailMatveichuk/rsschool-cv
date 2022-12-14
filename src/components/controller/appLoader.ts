076c2221ae7d6ceb5df8a1de5b7e4ba1147b419f
Mikhail <mihkailmatveichuk@gmail.com>
2022-12-14 22:32:05 +0300

    refactor: make variables urlLink and apiKeyString in appLoader.ts

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -1,10 +1,13 @@
 
 import Loader from './loader';
 
+const urlLink: string = 'https://nodenews.up.railway.app/';
+const apiKeyString: string = '9e58e025500941d3ab5fdca9b0e3b59c';
+
 class AppLoader extends Loader {
     constructor() {
-        super('https://nodenews.up.railway.app/', {
-            apiKey: '9e58e025500941d3ab5fdca9b0e3b59c', // получите свой ключ https://newsapi.org/
+        super(urlLink, {
+            apiKey: apiKeyString, // получите свой ключ https://newsapi.org/
         });
     }
 }
